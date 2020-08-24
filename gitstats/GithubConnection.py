0df3096488b391034e6878724b990d31e9606333
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2020-08-24 11:16:43 -0700

    Removing Group Name from the connection as well.

--- gitstats/GithubConnection.py
+++ gitstats/GithubConnection.py
@@ -1,11 +1,9 @@
 from github import Github
 
 class GithubConnection:
-    group_name: str
     repository_name: str
     
-    def __init__(self, group_name: str, access_token: str, repository_name: str):
-        self.group_name = group_name
+    def __init__(self, access_token: str, repository_name: str):
         self.repository_name = repository_name
         self.api = Github(access_token)
 
