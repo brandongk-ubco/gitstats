3069132f9024ecabf6a4271890e7ae90a0bdc7b6
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2020-09-02 20:26:23 -0700

    Filtering PRs on start and end date.

--- gitstats/PullRequestFinder.py
+++ gitstats/PullRequestFinder.py
@@ -1,10 +1,26 @@
-from . import GithubConnection
 import pandas as pd
 
 class PullRequestFinder:
 
-    def __init__(self, connection: GithubConnection):
-        self.connection = connection
+    def __init__(self, repository):
+        self.repository = repository
 
     def find(self, start, end):
-        return pd.DataFrame()
+        df = pd.DataFrame()
+
+        prs = self.repository.get_pulls(state="closed", sort="updated", direction="desc")
+        
+        for pr in prs:
+            if pr.updated_at > end:
+                continue
+            if pr.updated_at < start:
+                break
+
+            df = df.append({
+                "id": pr.number,
+                "date": pr.updated_at,
+                "assignee": "" if pr.assignee is None else pr.assignee.name,
+                "merged": pr.merged
+            }, ignore_index=True)
+
+        return df
