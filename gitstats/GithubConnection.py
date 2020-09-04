7cb47b219f0940ff47427d5e0763752a782e29d8
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2020-09-03 20:32:48 -0700

    Running formatting.

--- gitstats/GithubConnection.py
+++ gitstats/GithubConnection.py
@@ -1,8 +1,9 @@
 from github import Github
 
+
 class GithubConnection:
     repository_name: str
-    
+
     def __init__(self, access_token: str, repository_name: str):
         self.repository_name = repository_name
         self.api = Github(access_token)
--- gitstats/PullRequestFinder.py
+++ gitstats/PullRequestFinder.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 class PullRequestFinder:
 
     def __init__(self, repository):
@@ -8,19 +9,23 @@ class PullRequestFinder:
     def find(self, start, end):
         df = pd.DataFrame()
 
-        prs = self.repository.get_pulls(state="closed", sort="updated", direction="desc")
-        
+        prs = self.repository.get_pulls(state="closed",
+                                        sort="updated",
+                                        direction="desc")
+
         for pr in prs:
             if pr.updated_at > end:
                 continue
             if pr.updated_at < start:
                 break
 
-            df = df.append({
-                "id": pr.number,
-                "date": pr.updated_at,
-                "assignee": "" if pr.assignee is None else pr.assignee.name,
-                "merged": pr.merged
-            }, ignore_index=True)
+            df = df.append(
+                {
+                    "id": pr.number,
+                    "date": pr.updated_at,
+                    "assignee": "" if pr.assignee is None else pr.assignee.name,
+                    "merged": pr.merged
+                },
+                ignore_index=True)
 
         return df
