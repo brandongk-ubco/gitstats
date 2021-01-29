091f67af32655117e9518c6f8cf1f2da030c8449
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2021-01-28 20:01:54 -0800

    Incrementing version.

--- docs/examples/two_weeks.py
+++ docs/examples/two_weeks.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # Install with:
 # pip uninstall -y gitstats
-# pip install --no-cache-dir https://github.com/brandongk-ubco/gitstats/releases/download/v1.0.10/gitstats-1.0.10-py3-none-any.whl
+# pip install --no-cache-dir https://github.com/brandongk-ubco/gitstats/releases/download/v1.1.0/gitstats-1.1.0-py3-none-any.whl
 
 print("Running with gitstats version: {}".format(gitstats.__version__))
 
